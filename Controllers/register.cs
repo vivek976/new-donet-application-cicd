567388550e93e323f964634a6d0448812c525681
vivek976 <72372388+vivek976@users.noreply.github.com>
2024-06-15 14:59:24 +0530

    Update register.cs

--- Controllers/register.cs
+++ Controllers/register.cs
@@ -1,4 +1,4 @@
-/*﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetfordocker.Controllers
 {
@@ -12,32 +12,10 @@ namespace dotnetfordocker.Controllers
             _logger = logger;
         }
         [HttpGet]
-        public string RegistrationForm()
-        {
-            return " You are sucessfully signed up ";
-        }
-    }
-}*/
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
-namespace dotnetfordocker.Controllers
-{
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class RegisterController : ControllerBase
-    {
-        private readonly ILogger<RegisterController> _logger;
-
-        public RegisterController(ILogger<RegisterController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
         public IActionResult RegistrationForm()
         {
             return Ok("You are successfully signed up");
+            
         }
     }
 }
